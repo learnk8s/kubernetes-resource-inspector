cfb160a06aec3c2e83446a41087f1b85d26d67e2
Hussein Firas <me@husseinferas.com>
2022-08-28 11:21:21 +0300

    cluster detection

--- inspector.sh
+++ inspector.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-read -p "what is your k8s cluster? 
-(AKS,EKS,GKE): " cluster
-
-if ! [[ "$cluster" =~ ^(GKE|EKS|AKS|eks|gke|aks)$ ]]; then echo "cluster type is wrong";exit ; fi
+if [[ ! -z $(kubectl cluster-info dump | grep -e 'aws' -e 'eks') ]]
+then
+  cluster="EKS";
+elif [[ ! -z $(kubectl cluster-info dump | grep -e 'gcp' -e 'gke') ]]
+then
+  cluster="GKE"; 
+elif [[ ! -z $(kubectl cluster-info dump | grep -e 'azure' -e 'aks') ]]
+then
+  cluster="AKS";   
+else
+  echo "this cluster is not supported";exit;
+fi
 
 script=$(echo $cluster | tr '[:upper:]' '[:lower:]')".sh"
 
+echo "your are running $cluster cluster"
 echo "running $script ..."
+echo $'\n';
 
 /bin/bash $script
