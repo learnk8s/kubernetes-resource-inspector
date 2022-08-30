ee2aa50f18e1254998f9255f06052c8f8368e264
Hussein Feras <me@husseinferas.com>
2022-08-30 11:50:05 +0300

    add system reserved to EKS

--- eks.sh
+++ eks.sh
@@ -4,17 +4,17 @@ file="/host/etc/kubernetes/kubelet/kubelet-config.json"
 input="./.eks.json"
 kubectl debug "$node" -it  --image xxradar/hackon -- cat $file | tail -n +2 > $input;
 
-echo 'os reserved:'
-echo 'cpu=100m';
-echo 'memory=100Mi';
+echo 'system reserved:'
+echo "cpu="$(cat $input | jq -r '.systemReserved.cpu // 0');
+echo "memory="$(cat $input | jq -r '.systemReserved.memory // 0');
 echo $'\n';
 
 echo 'kubelet reserved:';
-echo "cpu="$(cat $input | jq -r '.kubeReserved.cpu');
-echo "memory="$(cat $input | jq -r '.kubeReserved.memory');
+echo "cpu="$(cat $input | jq -r '.kubeReserved.cpu // 0');
+echo "memory="$(cat $input | jq -r '.kubeReserved.memory // 0');
 echo $'\n';
 
 echo 'eviction threshold:';
-echo "memory="$(cat $input | jq -r '.evictionHard."memory.available"');
+echo "memory="$(cat $input | jq -r '.evictionHard."memory.available" // 0');
 
 rm $input;
